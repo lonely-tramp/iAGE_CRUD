6b0ebf18bbf704ad125fce262d820b8f2e2a6b8b
Konstantin <me@lonely-tramp.ru>
2021-08-12 17:51:30 +0300

    Refactoring

---
+++ iAGE_CRUD/EmployeeArgumentsParser.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iAGE_CRUD
+{
+    public class EmployeeArgumentsParser
+    {
+        public struct EmployeeArguments
+        {
+            public int? Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public decimal? SalaryPerHour { get; set; }
+
+            public bool IsValidForAdd()
+            {
+                var loosingArgs = new List<string>();
+
+                if (Id == null) loosingArgs.Add("Id");
+                if (string.IsNullOrWhiteSpace(FirstName)) loosingArgs.Add("FirstName");
+                if (string.IsNullOrWhiteSpace(LastName)) loosingArgs.Add("LastName");
+                if (SalaryPerHour == null) loosingArgs.Add("Salary");
+
+                var result = loosingArgs.Count < 4;
+                if (!result)
+                    Console.WriteLine($"Не задан аргумент {String.Join(", ", loosingArgs.ToArray())}");
+                return result;
+            }
+            public bool IsValidForUpdate()
+            {
+                var result = Id != null && !(string.IsNullOrWhiteSpace(FirstName) && string.IsNullOrWhiteSpace(FirstName) && SalaryPerHour == null);
+                if (!result)
+                    Console.WriteLine("Id и минимум один из FirstName, LastName, Salary должны быть заданы");
+                return result;
+            }
+            public bool IsValidForGetOrDelete()
+            {
+                var result = Id != null;
+                if (!result)
+                    Console.WriteLine("Id должен быть задан");
+                return result;
+            }
+        }
+
+        public static bool TryParseArguments(string[] args, out EmployeeArguments ea)
+        {
+            //var ea = new EmployeeArguments();
+            ea = default;
+            var result = true;
+            foreach (var arg in args)
+            {
+                char[] separators = { ':' };
+                var maxCountToSplit = 2;
+                var splittedArg = arg.Split(separators, maxCountToSplit);
+
+                if (splittedArg.Length < 2)
+                {
+                    Console.WriteLine($"Неверный аргумент {arg}");
+                    return false;
+                }
+
+                var name = splittedArg[0];
+                var value = splittedArg[1];
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine($"Неверное значение аргумента {arg}");
+                    return false;
+                }
+
+                switch (name)
+                {
+                    case "Id":
+                        if (int.TryParse(value, out int resultId) && resultId > 0)
+                            ea.Id = resultId;
+                        else
+                        {
+                            Console.WriteLine($"Неверное значение аргумента {arg}");
+                            result = false;
+                        }
+                        break;
+                    case "FirstName":
+                        ea.FirstName = value;
+                        break;
+                    case "LastName":
+                        ea.LastName = value;
+                        break;
+                    case "Salary":
+                        if (decimal.TryParse(value, NumberStyles.Number, CultureInfo.InvariantCulture, out var resultSalary))
+                            ea.SalaryPerHour = resultSalary;
+                        else
+                        {
+                            Console.WriteLine($"Неверное значение аргумента {arg}");
+                            result = false;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine($"Неверное название аргумента {arg}");
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
--- iAGE_CRUD/Program.cs
+++ iAGE_CRUD/Program.cs
@@ -18,121 +18,52 @@ namespace iAGE_CRUD
             args = args.Where((val, i) => i != 0)
                        .ToArray();
 
-            var isSuccesParsing = TryParseArguments(args, out var inputId, out var inputFirstName, out var inputLastName, out var inputSalary);
-
+            var isSuccesParsing = EmployeeArgumentsParser.TryParseArguments(args, out var ea);
             if (!isSuccesParsing) return;
 
-            //обработка операций
-            if ((operation == "-get" || operation == "-update" ||  operation == "-delete") && inputId == null)
-            {
-                Console.WriteLine("Не задан аргумент Id");
-                return;
-            }
-
-            var loosingArgs = new List<string>();
             var em = new EmployeesManager();
             switch (operation)
             {
                 case "-add":
-                    if (inputFirstName == null) loosingArgs.Add("FirstName");
-                    if (inputLastName == null) loosingArgs.Add("LastName");
-                    if (inputSalary == null) loosingArgs.Add("Salary");
-                    if (loosingArgs.Any())
-                    {
-                        Console.WriteLine($"Не задан аргумент {String.Join(", ", loosingArgs.ToArray())}");
+                    if (!ea.IsValidForAdd()) 
                         return;
-                    }
-                    var resultInsert = em.Insert(inputFirstName, inputLastName, (decimal)inputSalary);
+
+                    var resultInsert = em.Insert(ea.FirstName, ea.LastName, (decimal)ea.SalaryPerHour);
                     Console.WriteLine(resultInsert == null ? "Сотрудник не добавлен" : $"Запись о сотруднике добавлена: \n\r {resultInsert.GetInfo()}");
                     break;
                 case "-get":
-                    var foundEmployee = em.Get((int)inputId);
-                    Console.WriteLine(foundEmployee == null ? $"Сотрудник с Id={inputId} не найден" : $"{foundEmployee.GetInfo()}");
+                    if (!ea.IsValidForGetOrDelete())
+                        return;
+
+                    var foundEmployee = em.Get((int)ea.Id);
+                    Console.WriteLine(foundEmployee == null ? $"Сотрудник с Id={ea.Id} не найден" : $"{foundEmployee.GetInfo()}");
                     break;
                 case "-getall":
                     var resultGetall = em.Get();
-                    if (resultGetall == null || !resultGetall.Any()) Console.WriteLine("Сотрудники не найдены");
-                    else resultGetall.ForEach(e => Console.WriteLine(e.GetInfo()));
+                    if (resultGetall == null || !resultGetall.Any()) 
+                        Console.WriteLine("Сотрудники не найдены");
+                    else 
+                        resultGetall.ForEach(e => Console.WriteLine(e.GetInfo()));
                     break;
                 case "-update":
-                    if (inputFirstName == null && inputLastName == null && inputSalary == null)
-                    {
-                        Console.WriteLine("Как минимум одно поле должно быть изменено");
+                    if (!ea.IsValidForUpdate())
                         return;
-                    }
-                    var resultUpdate = em.Update((int)inputId, inputFirstName, inputLastName, inputSalary);
-                    Console.WriteLine(resultUpdate == null ? $"Сотрудник с Id={inputId} не найден" : $"Запись о сотруднике обновлена: \n\r {resultUpdate.GetInfo()}");
+
+                    var resultUpdate = em.Update((int)ea.Id, ea.FirstName, ea.LastName, ea.SalaryPerHour);
+                    Console.WriteLine(resultUpdate == null ? $"Сотрудник с Id={ea.Id} не найден" : $"Запись о сотруднике обновлена: \n\r {resultUpdate.GetInfo()}");
                     break;
                
                 case "-delete":
-                    var resultDelete = em.Remove((int)inputId);
-                    Console.WriteLine(resultDelete == null ? $"Сотрудник с Id={inputId} не найден" : $"Запись о сотруднике удалена: \n\r {resultDelete.GetInfo()}");
+                    if (!ea.IsValidForGetOrDelete())
+                        return;
+
+                    var resultDelete = em.Remove((int)ea.Id);
+                    Console.WriteLine(resultDelete == null ? $"Сотрудник с Id={ea.Id} не найден" : $"Запись о сотруднике удалена: \n\r {resultDelete.GetInfo()}");
                     break;
                 default:
                     Console.WriteLine("Доступны следующие команнды -add -update -delete -get -getall");
                     break;
-
             }
-        }
-        private static bool TryParseArguments(string[] args, out int? inputId, out string inputFirstName, out string inputLastName, out decimal? inputSalary)
-        {
-            inputId = null;
-            inputFirstName = null;
-            inputLastName = null;
-            inputSalary = null;
-
-            foreach (var arg in args)
-            {
-                Char[] separators = { ':' };
-                var maxCountToSplit = 2;
-                var splittedArg = arg.Split(separators, maxCountToSplit);
-
-                if (splittedArg.Length < 2)
-                {
-                    Console.WriteLine($"Неверный аргумент {arg}");
-                    return false;
-                }
-
-                var name = splittedArg[0];
-                var value = splittedArg[1];
-                if (string.IsNullOrEmpty(value))
-                {
-                    Console.WriteLine($"Неверное значение аргумента {arg}");
-                    return false;
-                }
-
-                switch (name)
-                {
-                    case "Id":
-                        if (int.TryParse(value, out int resultId) && resultId > 0)
-                            inputId = resultId;
-                        else
-                        {
-                            Console.WriteLine($"Неверное значение аргумента {arg}");
-                            return false;
-                        }
-                        break;
-                    case "FirstName":
-                        inputFirstName = value;
-                        break;
-                    case "LastName":
-                        inputLastName = value;
-                        break;
-                    case "Salary":
-                        if (decimal.TryParse(value, NumberStyles.Number, CultureInfo.InvariantCulture, out var resultSalary))
-                            inputSalary = resultSalary;
-                        else
-                        {
-                            Console.WriteLine($"Неверное значение аргумента {arg}");
-                            return false;
-                        }
-                        break;
-                    default:
-                        Console.WriteLine($"Неверное название аргумента {arg}");
-                        break;
-                }
-            }
-            return true;
         }
     }
 }

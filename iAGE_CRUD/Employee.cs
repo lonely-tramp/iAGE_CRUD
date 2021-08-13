3d6e8481a641ff1d0b56fee7921e64bcb9b764b3
Konstantin <me@lonely-tramp.ru>
2021-08-13 11:20:48 +0300

    Using Person

--- iAGE_CRUD/Employee.cs
+++ iAGE_CRUD/Employee.cs
@@ -1,15 +1,12 @@
 ﻿namespace iAGE_CRUD
 {
-    public class Employee
+    public class Employee : Person
     {
-        public Employee(int id, string firstName, string lastName, decimal salary) { Id = id; FirstName = firstName; LastName = lastName; SalaryPerHour = salary; }
+        public Employee(int id, string firstName, string lastName, decimal salary) : base(id , firstName, lastName) { SalaryPerHour = salary; } 
 
-        public int Id { get; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public decimal SalaryPerHour { get; set; }
 
-        public string GetInfo()
+        public override string GetInfo()
         {
             return $"Id = {Id},\tFirstName = {FirstName},\tLastName = {LastName},\tSalaryPerHour = {SalaryPerHour}";
         }
---
+++ iAGE_CRUD/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iAGE_CRUD
+{
+    public class Person
+    {
+        public Person(int id, string firstName, string lastName) { Id = id; FirstName = firstName; LastName = lastName; }
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual string GetInfo()
+        {
+            return $"Id = {Id},\tFirstName = {FirstName},\tLastName = {LastName}";
+        }
+    }
+}
